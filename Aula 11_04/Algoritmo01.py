d75986c6fa10de4ecb39e53dc53ee78afe69b6a5
Guilherme Alves de Campos <164068441+CampsGui@users.noreply.github.com>
2024-04-11 16:04:50 -0300

    Algoritmo01.py

--- Aula 11_04/Algoritmo01.py
+++ Aula 11_04/Algoritmo01.py
@@ -1,10 +1,20 @@
+contador=0
+
 J01 = int(input("Escolha um número entre 1 a 10: "))
-i=0
-J02=i
-if J01 <= 10:
-    while not J01 == J02:
-        J02 = int(input("Tente adivinhar o número que o jogador 1 escolheu(1 a 10): "))
-    if J02 == J01:
-        print("Parabéns, você adivinhou o número do jogador 1.")
-else:
-    print("Número Inválido")
+
+while J01 < 1 or J01 > 10:
+    print("\nPor favor, escolha um número entre 1 e 10.")
+    J01 = int(input("\nEscolha um número entre 1 a 10: "))
+
+J02=0
+
+while J01 != J02:
+    J02 = int(input("\nJogador 2, tente adivinhar o número que o jogador 1 escolheu (1 a 10): "))
+    contador += 1
+    
+    while J02 < 1 or J02 > 10:
+        print("\nPor favor, escolha um número entre 1 e 10.")
+        J02 = int(input("\nJogador 2, tente adivinhar o número que o jogador 1 escolheu (1 a 10): "))
+    
+print("\nParabéns, você adivinhou o número do jogador 1")
+print("Número de tentativas:", contador)
